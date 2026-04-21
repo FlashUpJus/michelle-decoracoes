283ddcba407833547725ce783525247d9068b6f5
Joloano <jolokpereira@gmail.com>
2026-04-21 12:19:18 -0300

    feat: adiciona seção de equipe com carrossel automático

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
 import MapSection from "../components/MapSection";
 import ProductSection from "../components/ProductSection";
+import Team from "../components/Team";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <Carousel />
       <ProductSection/>
       <MapSection />
+      <Team/>
     </>
   );
 }
